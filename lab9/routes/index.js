c96b24905b4d90b9688ddca07667843514967151
Colton Grange <cgrange92@gmail.com>
2016-03-23 03:16:00 +0000

    adding lab9 to repository

---
+++ lab9/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+/* Set up mongoose in order to connect to mongo database*/ 
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+var Comment = mongoose.model('Comment');
+
+router.post('/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+    res.json(comment);
+  });
+});
+
+/* GET comments from database */
+router.get('/comments', function(req, res, next) {
+  Comment.find(function(err, comments){
+    if(err){ return next(err); }
+    res.json(comments);
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/comments/:comment', function(req, res) {
+  res.json(req.comment);
+});
+
+router.put('/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+    res.json(comment);
+  });
+});
+
+module.exports = router;
